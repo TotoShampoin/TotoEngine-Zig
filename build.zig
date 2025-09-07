7f18cc82953c4480a22000fc2e509d03cd029469
TotoShampoin <thomas.garnier@totoshampoin.fr>
2025-09-07 14:57:42 +0200

    rearranged the build to separate the exe step

--- build.zig
+++ build.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const run_step = b.step("run", "Run the app");
+    const test_step = b.step("test", "Run tests");
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -32,63 +35,48 @@ pub fn build(b: *std.Build) void {
         },
     });
 
-    const exe = b.addExecutable(.{
-        .name = "TotoEngine_Zig",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-
-            .imports = &.{
-                .{ .name = "toto-engine", .module = mod },
-            },
-        }),
-    });
-
-    b.installArtifact(exe);
-
-    const run_step = b.step("run", "Run the app");
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
     const mod_tests = b.addTest(.{
         .root_module = mod,
     });
-
     const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
-    const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
 
-    const res_dir = b.path("res");
-    b.installDirectory(.{
-        .source_dir = res_dir,
-        .install_dir = .{ .custom = "bin/res" },
-        .install_subdir = "",
-    });
+    {
+        const exe = b.addExecutable(.{
+            .name = "TotoEngine_Zig",
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("src/main.zig"),
+                .target = target,
+                .optimize = optimize,
 
-    mod.addAnonymousImport(
-        "shader_vert",
-        .{ .root_source_file = compileShader(b, .vertex, "src/engine/assets/shader.vert", "src/engine/assets/shader.vert.spv") },
-    );
-    mod.addAnonymousImport(
-        "shader_frag",
-        .{ .root_source_file = compileShader(b, .fragment, "src/engine/assets/shader.frag", "src/engine/assets/shader.frag.spv") },
-    );
+                .imports = &.{
+                    .{ .name = "toto-engine", .module = mod },
+                },
+            }),
+        });
+        b.installArtifact(exe);
+
+        const run_cmd = b.addRunArtifact(exe);
+        run_step.dependOn(&run_cmd.step);
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const exe_tests = b.addTest(.{ .root_module = exe.root_module });
+        const run_exe_tests = b.addRunArtifact(exe_tests);
+        test_step.dependOn(&run_exe_tests.step);
+
+        const res_dir = b.path("res");
+        b.installDirectory(.{
+            .source_dir = res_dir,
+            .install_dir = .{ .custom = "bin/res" },
+            .install_subdir = "",
+        });
+
+        mod.addAnonymousImport("shader_vert", .{ .root_source_file = compileShader(b, .vertex, "src/engine/assets/shader.vert", "src/engine/assets/shader.vert.spv") });
+        mod.addAnonymousImport("shader_frag", .{ .root_source_file = compileShader(b, .fragment, "src/engine/assets/shader.frag", "src/engine/assets/shader.frag.spv") });
+    }
 }
 
 pub const ShaderStage = enum {
